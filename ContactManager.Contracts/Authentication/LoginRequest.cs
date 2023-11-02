3a82af98d3b609929c47f243f621147160916178
Mehdi Talebi <mahdi.tlby@gmail.com>
2023-11-02 07:48:19 +0100

    Initial Create

--- ContactManager.Contracts/Authentication/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace ContactManager.Contracts.Authentication;
-
-public record LoginRequest
-(
-    string Email,
-    string Password
-);
--- ContactManager.Contracts/Authentication/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace ContactManager.Contracts.Authentication;
-
-public record RegisterRequest
-(
-    string FirstName,
-    string LastName,
-    string Email,
-    string Password
-);
